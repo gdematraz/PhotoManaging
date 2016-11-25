03edd14980c717f68de678c0489a0457fac23cae
DEMATRAZ Grégoire <Gregoire.DEMATRAZ@cpnv.ch>
2016-11-25 15:52:56 +0100

    hello

---
+++ photo_managing (1).sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Ven 25 Novembre 2016 à 15:51
+-- Version du serveur :  10.1.16-MariaDB
+-- Version de PHP :  5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `photo_managing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `image`
+--
+
+CREATE TABLE `image` (
+  `p_id` int(11) NOT NULL,
+  `p_img` varchar(255) NOT NULL,
+  `p_title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `image`
+--
+
+INSERT INTO `image` (`p_id`, `p_img`, `p_title`) VALUES
+(2, 'img_upload/Jellyfish.jpg', 'Jellyfish.jpg'),
+(10, 'img_upload/Chrysanthemum.jpg', 'Chrysanthemum.jpg'),
+(11, 'img_upload/Hydrangeas.jpg', 'Hydrangeas.jpg'),
+(13, 'img_upload/Koala.jpg', 'Koala.jpg'),
+(17, 'img_upload/Tulips.jpg', 'Tulips.jpg'),
+(18, 'img_upload/Tulips.jpg', 'Tulips.jpg'),
+(19, 'img_upload/Lighthouse.jpg', 'chateau'),
+(20, 'img_upload/Penguins.jpg', 'Les beaux pingouins'),
+(21, 'img_upload/montagnes.jpg', 'montagne');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `firstname`, `lastname`) VALUES
+(1, 'gdematraz', '1234', 'GrÃ©goire', 'DÃ©matraz');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`p_id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `image`
+--
+ALTER TABLE `image`
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
